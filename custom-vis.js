27187b74f732b5666886b7f6205b74bb52a44069
agusBorges02 <aborges@correo.um.edu.uy>
2024-10-25 20:25:25 -0300

    test 2

--- custom-vis.js
+++ custom-vis.js
@@ -1,25 +1,36 @@
 export default {
   create: function(element, config) {
-    element.innerHTML = "<div id='custom-vis'>hola</div>";
+    // Crear un contenedor para la visualización
+    element.innerHTML = "<div id='custom-vis'>Cargando visualización...</div>";
   },
 
   updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
+    // Verificar si D3 está cargado
+    if (!window.d3) {
+      const script = document.createElement("script");
+      script.src = "https://d3js.org/d3.v5.min.js";
+      script.onload = () => this.updateAsync(data, element, config, queryResponse, details, doneRendering);
+      document.head.appendChild(script);
+      return;
+    }
+
     // Limpiar cualquier visualización previa
     d3.select("#custom-vis").html("");
 
-    // Configuración simple de SVG y rectángulo estático (sin datos)
+    // Crear un SVG con un rectángulo simple
     const svg = d3.select("#custom-vis")
                   .append("svg")
-                  .attr("width", "100%")
-                  .attr("height", 300);
+                  .attr("width", 400)
+                  .attr("height", 200);
 
     svg.append("rect")
        .attr("width", 200)
        .attr("height", 100)
-       .attr("x", 50)
+       .attr("x", 100)
        .attr("y", 50)
        .attr("fill", "steelblue");
 
+    // Llamar a doneRendering para notificar a Looker que terminó de renderizar
     doneRendering();
   }
 };
