a0adfdb7efb00c22a18cd14a94998113b96b1cd8
agusBorges02 <aborges@correo.um.edu.uy>
2024-10-25 20:37:35 -0300

    test 4

--- custom-vis.js
+++ custom-vis.js
@@ -1,36 +1,29 @@
-export default {
+looker.plugins.visualizations.add({
   create: function(element, config) {
-    // Crear un contenedor para la visualización
     element.innerHTML = "<div id='custom-vis'>Cargando visualización...</div>";
   },
 
   updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
-    // Verificar si D3 está cargado
-    if (!window.d3) {
-      const script = document.createElement("script");
-      script.src = "https://d3js.org/d3.v5.min.js";
-      script.onload = () => this.updateAsync(data, element, config, queryResponse, details, doneRendering);
-      document.head.appendChild(script);
-      return;
-    }
-
     // Limpiar cualquier visualización previa
-    d3.select("#custom-vis").html("");
+    const container = document.getElementById("custom-vis");
+    container.innerHTML = "";
 
-    // Crear un SVG con un rectángulo simple
-    const svg = d3.select("#custom-vis")
-                  .append("svg")
-                  .attr("width", 400)
-                  .attr("height", 200);
+    // Crear un SVG básico sin D3.js
+    const svgNS = "http://www.w3.org/2000/svg";
+    const svg = document.createElementNS(svgNS, "svg");
+    svg.setAttribute("width", "400");
+    svg.setAttribute("height", "200");
 
-    svg.append("rect")
-       .attr("width", 200)
-       .attr("height", 100)
-       .attr("x", 100)
-       .attr("y", 50)
-       .attr("fill", "steelblue");
+    const rect = document.createElementNS(svgNS, "rect");
+    rect.setAttribute("width", "200");
+    rect.setAttribute("height", "100");
+    rect.setAttribute("x", "100");
+    rect.setAttribute("y", "50");
+    rect.setAttribute("fill", "steelblue");
 
-    // Llamar a doneRendering para notificar a Looker que terminó de renderizar
+    svg.appendChild(rect);
+    container.appendChild(svg);
+
     doneRendering();
   }
-};
+});
