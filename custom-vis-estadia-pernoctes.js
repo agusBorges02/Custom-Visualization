56a56222d448925a6c30f77edf88feb618edacf4
agusBorges02 <aborges@correo.um.edu.uy>
2024-10-26 00:34:17 -0300

    estadia pernoctes v2

--- custom-vis-estadia-pernoctes.js
+++ custom-vis-estadia-pernoctes.js
@@ -1,22 +1,22 @@
 looker.plugins.visualizations.add({
     create: function(element, config) {
       element.innerHTML = `
-        <div id='custom-vis' style='display: flex; justify-content: space-around; background-color: #f3f1e3; padding: 20px; border-radius: 10px;'>
-          <div id="estadia-container" style="text-align: center; font-family: Arial, sans-serif; width: 45%;">
+        <div id='custom-vis' style='display: flex; justify-content: space-between; align-items: center; background-color: #f3f1e3; padding: 20px; border-radius: 10px;'>
+          <div id="estadia-container" style="text-align: center; font-family: Arial, sans-serif; width: 45%; padding: 20px;">
             <div style="display: flex; align-items: center; justify-content: center; gap: 10px;">
               <img src="https://img.icons8.com/?size=100&id=zyScOpkt9POE&format=png&color=000000" alt="Icono de Estadía" style="width:40px;"/>
               <div style="font-size: 18px; color: #333;">Estadía promedio</div>
             </div>
-            <div style="font-size: 36px; font-weight: bold; color: #333;">DÍAS</div>
-            <div id="estadia-value" style="font-size: 30px; color: #333;">0</div>
+            <div style="font-size: 20px; font-weight: bold; color: #333; margin-top: 5px;">DÍAS</div>
+            <div id="estadia-value" style="font-size: 36px; color: #333;">0</div>
           </div>
-          <div id="pernoctes-container" style="text-align: center; font-family: Arial, sans-serif; width: 45%;">
+          <div id="pernoctes-container" style="text-align: center; font-family: Arial, sans-serif; width: 45%; padding: 20px;">
             <div style="display: flex; align-items: center; justify-content: center; gap: 10px;">
               <img src="https://img.icons8.com/?size=100&id=11375&format=png&color=000000" alt="Icono de Pernoctes" style="width:40px;"/>
-              <div style="font-size: 18px; color: #333;">Pernoctes promedio</div>
+              <div style="font-size: 18px; color: #333;">Promedio Pernoctes</div>
             </div>
-            <div style="font-size: 36px; font-weight: bold; color: #333;">NOCHES</div>
-            <div id="pernoctes-value" style="font-size: 30px; color: #333;">0</div>
+            <div style="font-size: 20px; font-weight: bold; color: #333; margin-top: 5px;">NOCHES</div>
+            <div id="pernoctes-value" style="font-size: 36px; color: #333;">0</div>
           </div>
         </div>
       `;
@@ -30,12 +30,16 @@ looker.plugins.visualizations.add({
       // Asignar los valores a las etiquetas correspondientes en el HTML
       if (estadiaField) {
         const estadiaValue = data[0][estadiaField.name].value;
-        document.getElementById("estadia-value").innerText = estadiaValue.toFixed(1);
+        document.getElementById("estadia-value").innerText = estadiaValue ? estadiaValue.toFixed(1) : "N/A";
+      } else {
+        document.getElementById("estadia-value").innerText = "N/A";
       }
   
       if (pernoctesField) {
         const pernoctesValue = data[0][pernoctesField.name].value;
-        document.getElementById("pernoctes-value").innerText = pernoctesValue.toFixed(1);
+        document.getElementById("pernoctes-value").innerText = pernoctesValue ? pernoctesValue.toFixed(1) : "N/A";
+      } else {
+        document.getElementById("pernoctes-value").innerText = "N/A";
       }
   
       doneRendering();
